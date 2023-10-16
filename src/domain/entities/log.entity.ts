484f42400fd7c88d51004e7eadca0fc5e6648056
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-10-16 06:32:41 -0400

    log entity

---
+++ src/domain/entities/log.entity.ts
@@ -0,0 +1,20 @@
+
+export enum LogSeveretyLevel {
+  low    = 'low',
+  medium = 'medium',
+  high   =  'high'
+}
+
+export class LogEntity {
+
+  public level: LogSeveretyLevel; //enum
+  public message: string;
+  public createdAt: Date;
+
+  constructor( message: string, level: LogSeveretyLevel ){
+    this.message = message;
+    this.level   = level;
+    this.createdAt = new Date();
+  }
+
+}
