0159ff7300565898f9dd11c5fea115a666d56aba
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-10-16 20:37:16 -0400

    valid environment

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
+import { envs } from './config/plugins/envs.plugin';
 import { Server } from './presentation/server';
 
 ( async() => {
-  
   main();
-
 })();
 
 
 function main() {
-  Server.start();
+  // console.log({envs});
+  // Server.start();
 }
