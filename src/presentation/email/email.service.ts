ade80408d8540b320a90a411de044fd093811119
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-10-18 07:27:01 -0400

    nodemailer - send email

---
+++ src/presentation/email/email.service.ts
@@ -0,0 +1,70 @@
+import nodemailer from 'nodemailer'
+import { envs } from '../../config/plugins/envs.plugin'
+
+interface SendMailOptions {
+  to: string | string[];
+  subject: string;
+  htmlBody: string;
+  attachements?: Attachment[] 
+}
+
+interface Attachment {
+  filename: string;
+  path: string;
+}
+
+// TODO !!attachemtns
+
+export class EmailService {
+
+  private transporter = nodemailer.createTransport({
+    service: envs.MAILER_SERVICE,
+    auth: {
+      user: envs.MAILER_EMAIL,
+      pass: envs.MAILER_SECRET_KEY,
+    }
+    
+  });
+
+  async sendEmail( options: SendMailOptions ): Promise<boolean> {
+
+    const {to, subject, htmlBody, attachements = []} = options;
+
+
+    try {
+      const sendInformation = await this.transporter.sendMail({
+        to: to,
+        subject: subject,
+        html: htmlBody,
+        attachments: attachements
+      });
+
+      console.log(sendInformation);
+      return true;
+
+    } catch (error) {
+      console.log(error);
+      return false
+    }
+  }
+
+  async sendEmailWithFileSystemLogs(to: string | string[]) {
+    const subject = 'Services Log';
+    const htmlBody = `
+      <h3>Cron Reports services</h3>
+      <p>Testing...</p>
+      <p>See Reports</p>
+    `;
+
+    const attachements: Attachment[] = [
+      { filename: 'logs-all.log', path: './logs/logs-all.log'},
+      { filename: 'logs-high.log', path: './logs/logs-high.log'},
+      { filename: 'logs-medium.log', path: './logs/logs-medium.log'},
+    ]
+
+    return this.sendEmail({
+      to, subject, attachements, htmlBody
+    })
+  }
+
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,8 @@
-import { CronService } from '../cron/cron-service';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
+import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
@@ -13,19 +14,35 @@ export class Server {
   public static start() {
     console.log('Server started...');
 
-    CronService.createJob(
-      '*/5 * * * * *',
-      () => {
-        const url = 'https://google.com';
-        new CheckService(
-          fileSystemLogRepository,
+    // const emailService = new EmailService();
+    // emailService.sendEmail({
+    //   to: 'alejo.jesus.magne@gmail.com',
+    //   subject: 'Cron Logs Reports',
+    //   htmlBody: `
+    //     <h3>Cron Reports services</h3>
+    //     <p>Testing...</p>
+    //     <p>See Reports</p>
+    //   `
+    // });
 
-          () => console.log(`${url} is ok`),
-          (error: any) => console.log( error )
+    // const emailService = new EmailService();
+    // emailService.sendEmailWithFileSystemLogs([
+    //   'alejo.jesus.magne@gmail.com'
+    // ]);
+
+    // CronService.createJob(
+    //   '*/5 * * * * *',
+    //   () => {
+    //     const url = 'https://google.com';
+    //     new CheckService(
+    //       fileSystemLogRepository,
+
+    //       () => console.log(`${url} is ok`),
+    //       (error: any) => console.log( error )
         
-          ).execute( url );
-        // new CheckService().execute('http://localhost:3000');
-      }
-    );
+    //       ).execute( url );
+    //     // new CheckService().execute('http://localhost:3000');
+    //   }
+    // );
   }
 }
